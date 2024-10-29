d4f4f7bfba53a9c9aefe218a86fdd50a18f31763
venkat-ka <venkat.k.2516@gmail.com>
2024-10-29 13:54:50 +0530

    api gateway updates

--- v2microgateway/v2microgateway/src/main/java/com/v2gateway/v2microgateway/V2microgatewayApplication.java
+++ v2microgateway/v2microgateway/src/main/java/com/v2gateway/v2microgateway/V2microgatewayApplication.java
@@ -8,31 +8,46 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 import jakarta.ws.rs.HttpMethod;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-
 @EnableDiscoveryClient
 @SpringBootApplication
 public class V2microgatewayApplication {
 
-	
 	public static void main(String[] args) {
 		InetAddress ip;
-	    String hostname;
-		 try {
-	            ip = InetAddress.getLocalHost();
-	            hostname = ip.getHostName();
-	            System.out.println("Your current IP address : " + ip);
-	            System.out.println("Your current Hostname : " + hostname);
-	        } catch (UnknownHostException e) {
-	            e.printStackTrace();
-	        }
-        
-        
+		String hostname;
+		try {
+			ip = InetAddress.getLocalHost();
+			hostname = ip.getHostName();
+			System.out.println("Your current IP address : " + ip);
+			System.out.println("Your current Hostname : " + hostname);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
 		SpringApplication.run(V2microgatewayApplication.class, args);
+	}
+
+//	public WebMvcConfigurer configure() {
+//		
+//	}
+
+	@Bean
+	public WebFluxConfigurer configure() {
+		return new WebFluxConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry reg) {
+				reg.addMapping("/**").allowedOrigins("*");
+				
+			}
+		};
 	}
 
 	@Bean
@@ -42,9 +57,9 @@ public class V2microgatewayApplication {
 				.filters(f -> f.removeRequestHeader("Cookie").rewritePath("/users-ws/(?<segment>.*)", "/$\\{segment}")
 						.filter(authorizationHeaderFilter.apply(new AuthorizationHeaderFilter.Config())))
 				.uri("lb://users-ws"))
-				
+
 				.build();
-		
+
 //		return rlb.routes()
 //				.route("status_check", p -> p.path("/users-ws/users/status/check").and().method("GET")
 //				.filters(f -> f.removeRequestHeader("Cookie").rewritePath("/users-ws/(?<segment>.*)", "/$\\{segment}")
@@ -56,6 +71,5 @@ public class V2microgatewayApplication {
 //						.uri("lb://users-ws"))
 //				.build();
 	}
-	
-	
+
 }
